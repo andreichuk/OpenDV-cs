405e0ca102f07700ccd5db8d0a7ceec987448fc6
Cyril Andreichuk <2353790+andreichuk@users.noreply.github.com>
2022-05-19 21:29:37 +0300

    added the UI project

---
+++ src/OpenDV.UI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using OpenDV.Core.Configuration;
+
+internal static class App
+{
+    public static void Main()
+    {
+        var config = GetConfiguration();
+    }
+
+    private static AppConfig GetConfiguration()
+    {
+        var configurationSource =
+            new ConfigurationBuilder()
+                .AddJsonFile("config.json", optional: true)
+                .Build();
+
+        var basePath = Assembly.GetEntryAssembly()!.Location;
+
+        var config = ConfigurationReader.Get(configurationSource, basePath);
+        return config;
+    }
+}
