0173f8f84f34abca25e1603aaacaa271efb2eb3c
Cyril Andreichuk <2353790+andreichuk@users.noreply.github.com>
2022-05-23 11:06:51 +0300

    core: added Direction enum

---
+++ src/OpenDV.Core/Types.cs
@@ -0,0 +1,21 @@
+﻿namespace OpenDV.Core;
+
+public enum Direction : byte
+{
+    North = 0,
+    NorthNorthEast = 1,
+    NorthEast = 2,
+    NorthEastEast = 3,
+    East = 4,
+    SouthEastEast = 5,
+    SouthEast = 6,
+    SouthSouthEast = 7,
+    South = 8,
+    SouthSouthWest = 9,
+    SouthWest = 10,
+    SouthWestWest = 11,
+    West = 12,
+    NorthWestWest = 13,
+    NorthWest = 14,
+    NorthNorthWest = 15
+}
